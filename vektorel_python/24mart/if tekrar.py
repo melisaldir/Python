e4b72f3261bc86fb7f01497bed497a74fb96fc68
melisaldir <melisasaldir@gmail.com>
2024-03-24 12:23:37 +0300

    tekrar

---
+++ vektorel_python/24mart/if tekrar.py
@@ -0,0 +1,12 @@
+a = 5
+if (not (True and True)) or (not (False and False)): print("xx")
+else: print("yy")
+
+havaGuzel = True 
+saglikDurumu = True
+
+if havaGuzel == True:
+    if saglikDurumu == True:
+        print("Dışarı çıkabilirsin.")
+
+if havaGuzel == True and saglikDurumu == True: print("Dışarı çıkabilrsin.")

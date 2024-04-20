200b88ddd529094ccbbf1fe98c47a86aa57f41f8
melisaldir <melisasaldir@gmail.com>
2024-04-20 21:31:39 +0300

    20 nisan

---
+++ vektorel_python/Orta seviye konular/20 nisan/collections2 hash.py
@@ -0,0 +1,11 @@
+dizi ={}
+ogrenciler = []
+ogrenci ={}
+ogrenci1={}
+ogrenci1['adi'] = "Ali"
+ogrenci1['no'] = 235
+ogrenciler.append(ogrenci1)
+ogrenci['adi'] = "Barış"
+ogrenci['no'] = 332
+ogrenciler.append(ogrenci)
+print(ogrenciler)

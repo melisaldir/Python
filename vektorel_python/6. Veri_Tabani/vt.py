7a031c87d0d5b4207e0be955c9d046ee75242df2
melisaldir <melisasaldir@gmail.com>
2024-05-05 11:27:36 +0300

    5 mayıs

--- vektorel_python/6. Veri_Tabani/vt.py
+++ vektorel_python/6. Veri_Tabani/vt.py
@@ -17,11 +17,18 @@ class anaEkran(QMainWindow):
     icerik.addWidget(self.dugme1)
     self.dugme2 = QPushButton('Listele')
     icerik.addWidget(self.dugme2)
-    self.dugme3 = QPushButton('Sil')
+    self.dugme3 = QPushButton('Ara')
     icerik.addWidget(self.dugme3)
+    self.dugme4 = QPushButton('Sil')
+    icerik.addWidget(self.dugme4)
+    self.dugme5 = QPushButton('Düzelt')
+    icerik.addWidget(self.dugme5)
 
     self.dugme1.clicked.connect(self.ekle)
     self.dugme2.clicked.connect(self.listele)
+    self.dugme3.clicked.connect(self.ara)
+    self.dugme4.clicked.connect(self.sil)
+    self.dugme5.clicked.connect(self.duzelt)
 
     araclar = QWidget()
     araclar.setLayout(icerik)
@@ -29,14 +36,28 @@ class anaEkran(QMainWindow):
 
   def ekle(self):
     self.close()
-    self.listeleme = VeriListeEkranı('Kayıt Ekleme')
-    self.listeleme.show()
-
+    self.ekleme = EkleEkrani('Kayıt Ekleme')
+    self.ekleme.show()
 
   def listele(self):
     self.close()
-    self.ekleme = EkleEkrani('Kayıt Ekleme')
-    self.ekleme.show()
+    self.listeleme = VeriListeEkrani('Kayıt Ekleme')
+    self.listeleme.show()
+
+  def ara(self):
+    self.close()
+    self.arama = AramaEkrani('Kayıt Arama')
+    self.arama.show()
+
+  def sil(self):
+    self.close()
+    self.silme = SilmeEkrani('Kayıt Silme')
+    self.silme.show()
+
+  def duzelt(self):
+    self.close()
+    self.duzeltme = DuzeltmeEkrani('Kayıt Düzeltme')
+    self.duzeltme.show()
 
 
 
@@ -97,7 +118,6 @@ class EkleEkrani(QMainWindow):
     icerik.addWidget(self.edit1)
     icerik.addWidget(QLabel('Numara'))
     self.edit2 = QLineEdit()
-    self.edit2.setEchoMode(QLineEdit.EchoMode.Password)
     icerik.addWidget(self.edit2)
     self.dugme1 = QPushButton('Kaydet')
     icerik.addWidget(self.dugme1)
@@ -113,6 +133,7 @@ class EkleEkrani(QMainWindow):
     t2 = self.edit2.text()
     print("Edit 1 içeriği:", t1)
     print("Edit 2 içeriği:", t2)
+    
     import sqlite3
     vt = sqlite3.connect('rehber.db')
     svt = vt.cursor()
@@ -125,40 +146,237 @@ class EkleEkrani(QMainWindow):
     self.ae = anaEkran() # anaekran isimli pencere tanımla
     self.ae.show() # anaekran ı göster.
 
-class VeriListeEkranı(QMainWindow):
+class VeriListeEkrani(QMainWindow):
   def __init__(self,xx="Başlıksız"):
     super().__init__()
     self.setWindowTitle(xx)
 
-    icerik = QVBoxLayout()
-    icerik.addWidget(QLabel('Ad soyad:'))
-    icerik.addWidget(QLabel('Numara'))
-
-    self.d1= QPushButton("Ana ekrana döön")
-    icerik.addWidget(self.d1)
-    self.d1.clicked.connect(self.anaEkranaDön)
-
-    araclar = QWidget() 
-    araclar.setLayout(icerik)
-    self.setCentralWidget(araclar)
-
-    t1 = self.edit1.text()
-    t2 = self.edit2.text()
-    print("Edit 1 içeriği:", t1)
-    print("Edit 2 içeriği:", t2)
-
     import sqlite3
     vt = sqlite3.connect('rehber.db')
     svt = vt.cursor()
-    liste=svt.execute(f"SELECT * FROM isimler")
-    vt.commit()
+    liste = svt.execute(f"SELECT * FROM isimler")
+    
+    # icerik = QVBoxLayout()
+    icerik = QGridLayout()
+    x = 1
+    icerik.addWidget(QLabel('Adı'),0,1)
+    icerik.addWidget(QLabel('Numarası'),0,2)
+    for a in liste: 
+        print (a[1],a[2])
+        icerik.addWidget(QLabel(a[1]),x,1) # gridLayout taki x.satır ve 1.sütuna QLable yerleştir.
+        icerik.addWidget(QLabel(a[2]),x,2)
+        x+=1
+
+    # icerik.setColumnStretch(0, 2) # buna benzer bir şey ile hücre birleştirmesi yapılabilir.
+    self.d1 = QPushButton('Ana ekrana dön')
+    icerik.addWidget(self.d1,x,1) # x.satır ve 1.sütuna self.d1 widgetini yerleştir.
+    self.d1.clicked.connect(self.anaEkranaDon)
+
+    araclar = QWidget() # Pencere widgeti oluştur.
+    araclar.setLayout(icerik) # Pencere widgeti için layout ata
+    self.setCentralWidget(araclar) # pencere widgeti ana layatunu ata
     vt.close()
 
+  def anaEkranaDon(self):
     self.close() # mevcut pencereyi kapa
-    self.ae = anaEkran() # anaekran isimli pencere tanımla
+    self.ae = anaEkran('Ana ekran') # anaekran isimli pencere tanımla
     self.ae.show() # anaekran ı göster.
 
+class AramaEkrani(QMainWindow):
+  def anaEkranaDon(self):
+    self.close() # mevcut pencereyi kapa
+    self.ae = anaEkran('Ana ekran') # anaekran isimli pencere tanımla
+    self.ae.show() # anaekran ı göster.
 
+  def __init__(self,xx="Başlıksız"):
+    super().__init__()
+    self.setWindowTitle(xx)
+   
+    # icerik = QVBoxLayout()
+    self.icerik = QGridLayout()
+    self.silinecek = QLineEdit()
+    self.icerik.addWidget(self.silinecek,0,0)
+    getirB = QPushButton('Getir')
+    self.icerik.addWidget(getirB,1,0)
+    self.bulunanlar = []
+    getirB.clicked.connect(self.getir)
+    print("bulunanlar:",self.bulunanlar)
+    self.icerik.addWidget(QLabel('Id'),0,1)
+    self.icerik.addWidget(QLabel('Adı'),0,2)
+    self.icerik.addWidget(QLabel('Numarası'),0,3)
+    # adL = QLabel('...')
+    # nuL = QLabel('...')
+
+ 
+    # self.icerik.setColumnStretch(0, 2) # buna benzer bir şey ile hücre birleştirmesi yapılabilir.
+    self.d1 = QPushButton('Ana ekrana dön')
+    self.icerik.addWidget(self.d1,3,0) # x.satır ve 1.sütuna self.d1 widgetini yerleştir.
+    self.d1.clicked.connect(self.anaEkranaDon)
+
+    araclar = QWidget() # Pencere widgeti oluştur.
+    araclar.setLayout(self.icerik) # Pencere widgeti için layout ata
+    self.setCentralWidget(araclar) # pencere widgeti ana layatunu ata
+
+  def getir(self):
+    silinecekVeri = self.silinecek.text()
+    import sqlite3
+    vt = sqlite3.connect('rehber.db')
+    svt = vt.cursor()
+    gelen = svt.execute(f"SELECT * FROM isimler WHERE ad='{silinecekVeri}'")
+    x=1
+    for a in gelen:
+      print(a[0],a[1],a[2])
+      self.icerik.addWidget(QLabel(str(a[0])),x,1)
+      self.icerik.addWidget(QLabel(str(a[1])),x,2)
+      self.icerik.addWidget(QLabel(str(a[2])),x,3)
+      x+=1
+
+    vt.close()
+
+class SilmeEkrani(QMainWindow):
+  def anaEkranaDon(self):
+    self.close() # mevcut pencereyi kapa
+    self.ae = anaEkran('Ana ekran') # anaekran isimli pencere tanımla
+    self.ae.show() # anaekran ı göster.
+
+  def __init__(self,xx="Başlıksız"):
+    super().__init__()
+    self.setWindowTitle(xx)
+   
+    # icerik = QVBoxLayout()
+    self.icerik = QGridLayout()
+    self.silinecek = QLineEdit()
+    self.icerik.addWidget(self.silinecek,0,0)
+    getirB = QPushButton('Getir')
+    self.icerik.addWidget(getirB,1,0)
+    self.bulunanlar = []
+    getirB.clicked.connect(self.getir)
+    print("bulunanlar:",self.bulunanlar)
+    self.icerik.addWidget(QLabel('Id'),0,1)
+    self.icerik.addWidget(QLabel('Adı'),0,2)
+    self.icerik.addWidget(QLabel('Numarası'),0,3)
+    # adL = QLabel('...')
+    # nuL = QLabel('...')
+
+    self.silinecekId = QLineEdit()
+    self.icerik.addWidget(self.silinecekId,3,0)
+    self.silB = QPushButton('Sil')
+    self.icerik.addWidget(self.silB,4,0)
+    self.silB.clicked.connect(self.sil)
+
+ 
+    # self.icerik.setColumnStretch(0, 2) # buna benzer bir şey ile hücre birleştirmesi yapılabilir.
+    self.d1 = QPushButton('Ana ekrana dön')
+    self.icerik.addWidget(self.d1,5,0) # x.satır ve 1.sütuna self.d1 widgetini yerleştir.
+    self.d1.clicked.connect(self.anaEkranaDon)
+
+    araclar = QWidget() # Pencere widgeti oluştur.
+    araclar.setLayout(self.icerik) # Pencere widgeti için layout ata
+    self.setCentralWidget(araclar) # pencere widgeti ana layatunu ata
+
+  def getir(self):
+    silinecekVeri = self.silinecek.text()
+    import sqlite3
+    vt = sqlite3.connect('rehber.db')
+    svt = vt.cursor()
+    gelen = svt.execute(f"SELECT * FROM isimler WHERE ad='{silinecekVeri}'")
+    x=1
+    silinecek = []
+    for a in gelen:
+      print(a[0],a[1],a[2])
+      self.icerik.addWidget(QLabel(str(a[0])),x,1)
+      self.icerik.addWidget(QLabel(str(a[1])),x,2)
+      self.icerik.addWidget(QLabel(str(a[2])),x,3)
+      silinecek.append(str(a[0]))
+      x+=1
+  
+    vt.close()
+
+  def sil(self):
+    import sqlite3
+    vt = sqlite3.connect('rehber.db')
+    svt = vt.cursor()
+    print("self.silinecekId.text()",self.silinecekId.text())
+    svt.execute(f"DELETE FROM isimler WHERE id='{self.silinecekId.text()}'")
+    vt.commit()
+    vt.close()
+    self.close()
+    self.liste = VeriListeEkrani()
+    self.liste.show()
+        
+class DuzeltmeEkrani(QMainWindow):
+  def anaEkranaDon(self):
+    self.close() # mevcut pencereyi kapa
+    self.ae = anaEkran('Ana ekran') # anaekran isimli pencere tanımla
+    self.ae.show() # anaekran ı göster.
+
+  def __init__(self,xx="Başlıksız"):
+    super().__init__()
+    self.setWindowTitle(xx)
+   
+    # icerik = QVBoxLayout()
+    self.icerik = QGridLayout()
+    self.silinecek = QLineEdit()
+    self.icerik.addWidget(self.silinecek,0,0)
+    getirB = QPushButton('Getir')
+    self.icerik.addWidget(getirB,1,0)
+
+    getirB.clicked.connect(self.getir)
+    # print("bulunanlar:",self.bulunanlar)
+    self.icerik.addWidget(QLabel('Id'),0,1)
+    self.icerik.addWidget(QLabel('Adı'),0,2)
+    self.icerik.addWidget(QLabel('Numarası'),0,3)
+    self.duzelecekId = QLineEdit("Düzelcek id")
+    self.icerik.addWidget(self.duzelecekId,3,0)
+
+    self.yeniAd = QLineEdit("yeni ad")
+    self.icerik.addWidget(self.yeniAd,4,0)
+    self.yeniNumara = QLineEdit("yeni numara")
+    self.icerik.addWidget(self.yeniNumara,5,0)
+
+    self.silB = QPushButton('Düzelt')
+    self.icerik.addWidget(self.silB,6,0)
+    self.silB.clicked.connect(self.duzelt)
+
+    self.d1 = QPushButton('Ana ekrana dön')
+    self.icerik.addWidget(self.d1,7,0) # x.satır ve 1.sütuna self.d1 widgetini yerleştir.
+    self.d1.clicked.connect(self.anaEkranaDon)
+
+    araclar = QWidget() # Pencere widgeti oluştur.
+    araclar.setLayout(self.icerik) # Pencere widgeti için layout ata
+    self.setCentralWidget(araclar) # pencere widgeti ana layatunu ata
+
+  def getir(self):
+    silinecekVeri = self.silinecek.text()
+    import sqlite3
+    vt = sqlite3.connect('rehber.db')
+    svt = vt.cursor()
+    gelen = svt.execute(f"SELECT * FROM isimler WHERE ad='{silinecekVeri}'")
+    x=1
+    silinecek = []
+    for a in gelen:
+      print(a[0],a[1],a[2])
+      self.icerik.addWidget(QLabel(str(a[0])),x,1)
+      self.icerik.addWidget(QLabel(str(a[1])),x,2)
+      self.icerik.addWidget(QLabel(str(a[2])),x,3)
+      silinecek.append(str(a[0]))
+      x+=1
+  
+    vt.close()
+
+  def duzelt(self):
+    import sqlite3
+    vt = sqlite3.connect('rehber.db')
+    svt = vt.cursor()
+    print("self.duzelecekId.text()",self.duzelecekId.text())
+    # svt.execute(f"DELETE FROM isimler WHERE id='{self.duzelecekId.text()}'")
+    svt.execute(f"UPDATE isimler SET ad = '{self.yeniAd.text()}', nu = '{self.yeniNumara.text()}' WHERE id = '{self.duzelecekId.text()}'")
+    vt.commit()
+    vt.close()
+    self.close()
+    self.liste = VeriListeEkrani()
+    self.liste.show()
+        
 sifreOlustur()
 uygulama = QApplication([])
 # pencere = loginPenceresi("Giriş1")

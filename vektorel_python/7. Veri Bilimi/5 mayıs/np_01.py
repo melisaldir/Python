c3a2a3d9a93e4e5740571b26897b983c59118675
melisaldir <melisasaldir@gmail.com>
2024-05-05 13:16:25 +0300

    5 Mayıs

---
+++ vektorel_python/7. Veri Bilimi/5 mayıs/np_01.py
@@ -0,0 +1,3 @@
+import numpy as np
+xxx = np.array([1,2,3])
+print(xxx)
---
+++ vektorel_python/7. Veri Bilimi/5 mayıs/np_02.py
@@ -0,0 +1,12 @@
+import numpy as np
+arr1 = np.zeros((3,4))
+print(arr1)
+
+arr2 = np.ones((3,5,2))
+print(arr2)
+
+arr3 = np.zeros((3,5))*4
+print(arr3)
+
+
+
